6fcfac9dda2b4fa1ac6aa40659e46ce7bf63837a
mmondani <mmondani89@gmail.com>
2019-07-27 16:37:43 -0300

    Se implementa la clase PicsFusesModel que se encarga de leer el archivo config_pics.json, parsearlo y cargar dos Map con los pics-configuraciones y configuraciones-fusibles

--- src/main/java/PicsFusesModel.java
+++ src/main/java/PicsFusesModel.java
@@ -10,13 +10,15 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PicsFusesModel {
     static private PicsFusesModel mInstance = null;
-    private Map<String, List<Fuse>> picFusesConfigurations;
+    private Map<String, String> picsConfigsMap;
+    private Map<String, List<Fuse>> configsFusesMap;
 
 
     private PicsFusesModel () {
@@ -24,11 +26,42 @@ public class PicsFusesModel {
             Path path = Paths.get(PicsFusesModel.class.getResource("/main/resources/config_pics.json").toURI());
             String jsonFileConent = new String(Files.readAllBytes(path));
 
-            picFusesConfigurations = new HashMap<>();
+            picsConfigsMap = new HashMap<>();
+            configsFusesMap = new HashMap<>();
 
             JSONObject obj = new JSONObject(jsonFileConent);
             JSONObject picsObject = obj.getJSONObject("pics");
             JSONObject configsObject = obj.getJSONObject("configs");
+
+
+            for (String key : picsObject.keySet()) {
+                picsConfigsMap.put(key, picsObject.getString(key));
+            }
+
+            for (String key : configsObject.keySet()) {
+                JSONArray fusesArray =  configsObject.getJSONArray(key);
+
+                List<Fuse> fusesList = new ArrayList<>();
+                for (int i = 0; i < fusesArray.length(); i++) {
+                    JSONObject fuseObject = fusesArray.getJSONObject(i);
+
+
+                    JSONArray valuesArray = fuseObject.getJSONArray("values");
+                    List<String> valuesList = new ArrayList<>();
+                    for (int j = 0; j < valuesArray.length(); j++) {
+                        valuesList.add(valuesArray.getString(j));
+                    }
+
+                    fusesList.add(new Fuse(fuseObject.getString("name"),
+                            fuseObject.getString("description"),
+                            fuseObject.getInt("word"),
+                            fuseObject.getInt("offset"),
+                            fuseObject.getInt("bits"),
+                            valuesList));
+                }
+
+                configsFusesMap.put(key, fusesList);
+            }
         }
         catch (IOException ioe) {
             ioe.printStackTrace();
@@ -47,6 +80,26 @@ public class PicsFusesModel {
     }
 
 
+    public List<String> getPics () {
+        List<String> picsList = new ArrayList<>(picsConfigsMap.size());
+
+        for (String key : picsConfigsMap.keySet()) {
+            picsList.add(key);
+        }
+
+        return picsList;
+    }
+
+    
+    public List<Fuse> getPicFuses (String pic) {
+        String config = picsConfigsMap.get(pic);
+
+        if (config != null) {
+            return configsFusesMap.get(config);
+        }
+
+        return null;
+    }
 
 
     private class Fuse {
